e47aa6721efe3211d53ae5221629a3806bafba12
Laiz <laizsc.89@gmail.com>
2023-11-01 17:31:23 -0300

    Corrige arq html, troca de pág e add pág de result

--- script.js
+++ script.js
@@ -1,14 +1,15 @@
 let pgtAtual = 1;
+let resCerta = 0;
 
 function onFormDadosSubmit(evt) {
     evt.preventDefault();
 
-    const form = evt.target;
-    const formData = new FormData(form);
+    const formD = evt.target;
+    const formData = new FormData(formD);
 
     console.log(formData.getAll('personagens'));
 
-    const urlApi = "http://localhost:8080/player";
+    const urlApi = "http://localhost:8080/user";
     const requestParams = {
         method: 'POST',
         headers: {
@@ -19,27 +20,88 @@ function onFormDadosSubmit(evt) {
             genero: formData.get('genero')
         })
     };
+
+}
+
+function onFormQuizSubmit(evt) {
+    evt.preventDefault();
+
+    const formQ = evt.target;
+    const formDataQ = new FormData(formQ)
+
+    const urlApi = "http://localhost:8080/quiz";
+    const requestParams = {
+        method: 'POST',
+        headers: {
+            'contenty-type': 'application/json'
+        },
+        body: JSON.stringify({
+            p1: formDataQ.get(p1),
+            p2: formDataQ.get(p2),
+            p3: formDataQ.get(p3),
+            p4: formDataQ.get(p4),
+            p5: formDataQ.get(p5)
+        })
+    };
+
+    for(const key of formDataQ.keys()) {
+        const res = formDataQ.get(key);
+
+        if (res === 'true') {
+            resCerta++;
+        }
+    }
+
+    console.log(resCerta);
 }
 
 function onInputChange(evt) {
     console.log(evt.target.value);
 }
 
-function passarParaFormQuiz() {
-    document.getElementById('dados').style.display = 'none';
-    document.getElementById('quiz').style.display = 'block';
+function salvarDadosForm() {
+
+    document.querySelector('#quiz').style.display = 'block';
+    document.querySelector('#dados').style.display = 'none';
 }
 
 function pgtAnterior(evt) {
     evt.preventDefault();
-    document.getElementById('p' + pgtAtual).style.display = 'none';
+   
+    document.querySelector('#p' + pgtAtual).style.display = 'none';
     pgtAtual--;
-    document.getElementById('p' + pgtAtual).style.display = 'block';
+    document.querySelector('#p' + pgtAtual).style.display = 'block';   
 }
 
 function pgtPosterior(evt) {
     evt.preventDefault();
-    document.getElementById('p' + pgtAtual).style.display = 'none';
+
+    document.querySelector('#p' + pgtAtual).style.display = 'none';
     pgtAtual++;
-    document.getElementById('p' + pgtAtual).style.display = 'block';
+    document.querySelector('#p' + pgtAtual).style.display = 'block';
+        
+    if (pgtAtual !== 5) {
+        
+        document.querySelector('#btn-depois').style.display = 'inline-block';
+        document.querySelector('#btn-antes').style.display = 'inline-block';
+                     
+    } else {
+
+        document.querySelector('#btn-depois').style.display = 'none';
+        document.querySelector('#btn-antes').style.display = 'none';
+        document.querySelector('#btn-btn-salvar').style.display = 'block';
+        
+    }
+    
+
+}
+
+function mostrarResultado(evt) {
+    evt.preventDefault();
+    
+    const saudacaoResultado = document.querySelector('#saudacao'); 
+    
+    document.querySelector('#quiz').style.display = 'none';
+    document.querySelector('#resultado').style.display = 'block';
+
 }
