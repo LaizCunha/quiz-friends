d08ebe22429707052a39da2c515fdfa2bdcfd8d8
Laiz <laizsc.89@gmail.com>
2023-11-03 14:14:20 -0300

    Refatorando captura de dados do formulário

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
+// Declaração de variáveis globais
+
 let pgtAtual = 1;
 let resCerta = 0;
+let dados = {
+    nome: "",
+    genero: "",
+    personagens: []
+};
+let quiz = {
+    p1: "",
+    p2: "",
+    p3: "",
+    p4: "",
+    p5: ""
+};
+
+// Formulário de Dados
 
 function onFormDadosSubmit(evt) {
     evt.preventDefault();
@@ -7,41 +23,32 @@ function onFormDadosSubmit(evt) {
     const formD = evt.target;
     const formData = new FormData(formD);
 
-    console.log(formData.getAll('personagens'));
-
-    const urlApi = "http://localhost:8080/user";
-    const requestParams = {
-        method: 'POST',
-        headers: {
-            'contenty-type': 'application/json'
-        },
-        body: JSON.stringify({
-            nome: formData.get('nome'),
-            genero: formData.get('genero')
-        })
+    dados = {
+        nome: formData.get('nome'),
+        genero: formData.get('genero'),
+        personagens: formData.get('personagens')
     };
 
+    document.querySelector('#quiz').style.display = 'block';
+    document.querySelector('#dados').style.display = 'none';
+
+    console.log(dados);
 }
+
+// Formulário do Quiz
 
 function onFormQuizSubmit(evt) {
     evt.preventDefault();
 
     const formQ = evt.target;
-    const formDataQ = new FormData(formQ)
+    const formDataQ = new FormData(formQ);
 
-    const urlApi = "http://localhost:8080/quiz";
-    const requestParams = {
-        method: 'POST',
-        headers: {
-            'contenty-type': 'application/json'
-        },
-        body: JSON.stringify({
-            p1: formDataQ.get(p1),
-            p2: formDataQ.get(p2),
-            p3: formDataQ.get(p3),
-            p4: formDataQ.get(p4),
-            p5: formDataQ.get(p5)
-        })
+    quiz = {
+        p1: formDataQ.get('p1'),
+        p2: formDataQ.get('p2'),
+        p3: formDataQ.get('p3'),
+        p4: formDataQ.get('p4'),
+        p5: formDataQ.get('p5')
     };
 
     for(const key of formDataQ.keys()) {
@@ -53,29 +60,44 @@ function onFormQuizSubmit(evt) {
     }
 
     console.log(resCerta);
+    console.log(quiz);
+
+    document.querySelector('#quiz').style.display = 'none';
+    document.querySelector('#resultado').style.display = 'block';
+
+
+    // Página de resultado final
+          
+    if (resCerta >= 3) {
+        document.querySelector('#saudacao').innerHTML = `Parabéns, ${dados.nome}! Você é realmente fã!`;
+    } else {
+        document.querySelector('#saudacao').innerHTML = `Foi por pouco, ${dados.nome}! Tente novamente!`;
+    }
+
+    document.querySelector('#acertos').innerHTML = `Você teve ${resCerta / 5 * 100}% de acerto.`;   
+        
 }
 
-function onInputChange(evt) {
-    console.log(evt.target.value);
-}
-
-function salvarDadosForm() {
-
-    document.querySelector('#quiz').style.display = 'block';
-    document.querySelector('#dados').style.display = 'none';
-}
+// Botão para página anterior
 
 function pgtAnterior(evt) {
     evt.preventDefault();
    
     document.querySelector('#p' + pgtAtual).style.display = 'none';
     pgtAtual--;
-    document.querySelector('#p' + pgtAtual).style.display = 'block';   
+    document.querySelector('#p' + pgtAtual).style.display = 'block';
+
+    if (pgtAtual >= 0) {
+        document.querySelector('#btn-antes').style.display = 'none';
+    }
+    
+    
 }
 
-function pgtPosterior(evt) {
-    evt.preventDefault();
+// Botão para página posterior
 
+function pgtPosterior(evt) {
+   
     document.querySelector('#p' + pgtAtual).style.display = 'none';
     pgtAtual++;
     document.querySelector('#p' + pgtAtual).style.display = 'block';
@@ -93,15 +115,5 @@ function pgtPosterior(evt) {
         
     }
     
-
 }
 
-function mostrarResultado(evt) {
-    evt.preventDefault();
-    
-    const saudacaoResultado = document.querySelector('#saudacao'); 
-    
-    document.querySelector('#quiz').style.display = 'none';
-    document.querySelector('#resultado').style.display = 'block';
-
-}
